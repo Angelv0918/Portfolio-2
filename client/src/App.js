87b2982f8db843469a0e9f8108a81cc7ecedbab7
angelv18 <angelvasquez0918@gmail.com>
2022-12-26 14:34:02 -0500

    portfolio

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import Profile from './PortfolioContainer/Home/Profile.js'
+import './App.css';
+
+const App = () => {
+  return <div className='App'>
+  <Profile />
+  </div>
+}
+
+export default App;
---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,70 @@
+import React from "react";
+import Typical from "react-typical";
+import './Profile.css'
+
+export default function Profile() {
+  return (
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+            <a href="https://github.com/Angelv0918">
+              <i className="fa fa-github-square"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/angel-vasquez0918/">
+              <i className="fa fa-linkedin-square"></i>
+            </a>
+            <a href="https://twitter.com/AngelVa18312065">
+              <i className="fa fa-twitter-square"></i>
+            </a>
+          </div>
+          </div>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'm <span className="highlighted-text">Angel</span>
+            </span>
+          </div>
+          <div className="profile-details-role">
+          <span className="primary-text">
+            {" "}
+            <h1>
+              <Typical
+                loop={Infinity}
+                steps={[
+                  "Enthusiastic Dev 🔴",
+                  1000,
+                  "Full Stack Developer 💻",
+                  1000,
+                  "MERN Dev 😎",
+                  1000,
+                  "Python Dev 🐍",
+                  1000,
+                  "React/React Native Dev📱",
+                  1000,
+                ]}
+              />
+            </h1>
+            <span className="profile-role-tagline">
+              Self starter with a knack for building applications with front and
+              back end operations.
+            </span>
+          </span>
+        </div>
+        <div className="profile-options">
+          <button className="btn primary-btn"> Hire Me </button>
+          <a href="Angel-resume.pdf" download="Angel Angel-resume.pdf">
+            <button className="btn highlighted-btn">Get Resume</button>
+          </a>
+        </div>
+      </div>
+      <div className="profile-picture">
+        <div className="profile-picture-background">
+            
+        </div>
+      </div>
+    </div>
+    </div>
+  );
+}
